9413d09eb37f832334e1f3a71dc72545f917ae09
Jason Zhang <jason_zhang2@comcast.com>
2017-10-29 21:43:45 -0600

    update path

--- csci5573/main.cpp
+++ csci5573/main.cpp
@@ -16,18 +16,18 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::string fileContent = "my text here and content!!!";
     std::chrono::milliseconds begin = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-    for (int i=1; i<=100; i++) {
+    for (int i=1; i<=1000; i++) {
         std::string filename = "title_" + std::to_string(i) + ".txt";
-        std::ofstream outfile("/Users/jzhang201/csci5537/" + filename);
+        std::ofstream outfile("/Users/jzhang201/csci5573/" + filename);
         outfile << fileContent << std::endl;
         outfile.close();
     }
     for (int i=1; i<=1000; i++) {
         std::string filename = "title_" + std::to_string(i) + ".txt";
-        std::remove(("/Users/jzhang201/csci5537/" + filename).c_str());
+        std::remove(("/Users/jzhang201/csci5573/" + filename).c_str());
     }
     std::chrono::milliseconds end = std::chrono::duration_cast< std::chrono::milliseconds >(std::chrono::system_clock::now().time_since_epoch());
-    
+
     std::cout << "begin timestamp " << begin.count();
     std::cout << "\n";
     std::cout << "end timestamp " << end.count();
